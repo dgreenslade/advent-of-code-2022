18bf09f91e3c7bc843ee74121bdc216ee5515b02
Dominic Greenslade <greenslade.dominic@gmail.com>
2022-12-07 06:32:49 +1100

    Minor tidy

--- d06.py
+++ d06.py
@@ -1,15 +1,19 @@
 def input_datastream(file:str)->str:
-    """Return nested list of integers"""
+    """File is one long string of random characters"""
     with open(file) as f:
         datastream = f.read()
     return datastream
 
-def identify_marker(datastream:str,set_size:int):
-    """identifies idx & unique slices of strings"""
+def identify_marker(datastream:str,set_size:int)-> tuple:
+    """
+    iterates through string and identifies first instance where there 
+    are {set_size} unique characters (the packet/message marker) and the 
+    index after that (the beginning of the content)
+    """
     i = 0
     while i < len(datastream):
         marker = datastream[i:i+set_size]
-        if len(marker) == len(set(marker)):
+        if len(set(marker)) == set_size:
             return (i+set_size), marker
         else:
             i += 1
@@ -17,22 +21,15 @@ def identify_marker(datastream:str,set_size:int):
 def main():
     datastream = input_datastream(r'./data/d06.txt')
 
-    # Sample data
-    # tst = 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'
-    # index, marker = identify_marker(tst, 4)
-    # print(f'Number of characters for start of packet: {index}')
-    # print(f'Marker content: {marker}')
+    # Length of markers for both part 1 and part 2
+    p1_marker_len = 4
+    p2_marker_len = 14
 
-    # Part 1 marker
-    index, marker = identify_marker(datastream, 4)
-    print(f'---4 repeating characters \nNumber of chars until start of packet {index}')
-    print(f'Marker content: {marker}')
-
-    # Part 2 marker 
-    index, marker = identify_marker(datastream, 14)
-    print(f'---14 repeating characters \nNumber of chars until start of message {index}')
-    print(f'Marker content: {marker}')
-
+    for x in [p1_marker_len, p2_marker_len]:
+        # Print both part results
+        index, marker = identify_marker(datastream, x)
+        print(f'---{x} repeating characters \nNumber of chars until start of packet {index}')
+        print(f'Marker content: {marker}')
 
 if __name__ == '__main__':
     main()
